48cf9de2eeadad143668efe4dea9d48caaa3cc55
ARYAN C R <57360045+aryancrajan@users.noreply.github.com>
2023-07-18 06:37:40 +0530

    without builtin function

--- functions/fn2.js
+++ functions/fn2.js
@@ -1,5 +1,19 @@
-function diffNumbers(a,b){
-    return Math.abs(a-b);}
+// function diffNumbers(a,b){
+//     return Math.abs(a-b);}
 var a=5;
 var b=8;
-console.log(diffNumbers(a,b));
+
+function diff(a,b)
+    {
+    return a-b
+    }
+
+function abs(c)
+    {
+    if(c>0)
+        return c
+    else
+        return -1*c
+    }
+c=diff(a,b)
+console.log(abs(c))
