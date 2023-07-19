a8d80ee43d3af5d334411c00eb0b135d43fc6d61
ARYAN C R <57360045+aryancrajan@users.noreply.github.com>
2023-07-19 11:54:48 +0530

    Solved without using functions

--- 2d arrays/2d-2.js
+++ 2d arrays/2d-2.js
@@ -1,5 +1,15 @@
 const a=[100,20,31,150,39,72];
-var low=Math.min(...a);
-var high=Math.max(...a);
+// var low=Math.min(...a);
+// var high=Math.max(...a);
+var low=a[0];
+var high=a[0];
+for(let i of a)
+    {
+    if(i<low)
+        low=i
+    if(i>high)
+        high=i
+    }
+
 console.log('Highest value:',high,'and lowest value:',low);
 
